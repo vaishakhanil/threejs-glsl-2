ca048b1c26922b89333f3da6f00867ad9d1c397b
vaishakhanil <vaishakh.anil.360@gmail.com>
2020-07-02 09:07:39 +0530

    Code Cleanup

--- script.js
+++ script.js
@@ -1,69 +1,58 @@
-let container;
-let camera, scene, renderer;
-let uniforms;
-let material;
-let geometry;
 let mesh;
-init();
-animate();
 
-function init() {
-    container = document.getElementById( 'container' );
+/* Scene Setup */
+let container = document.getElementById( 'container' );
+let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 10;
+let scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    camera.position.z = 10;
+/* Defining Uniforms*/
+uniforms = {
+    "time": { value: 1.0 }
+};
 
-    scene = new THREE.Scene();
-    uniforms = {
-        "time": { value: 1.0 }
-    };
+/* Text Geometry Setup*/
+let loader = new THREE.FontLoader();
+loader.load('fonts/Poppins_Regular.json',function(font){
+    let geometry = new THREE.TextGeometry('VAISHAKH',{font:font,size:2,height:0.8,bevelEnabled:false});
 
-    let loader = new THREE.FontLoader();
-    loader.load('fonts/Poppins_Regular.json',function(font){
-    geometry = new THREE.TextGeometry('VAISHAKH',{font:font,size:2,height:1,bevelEnabled:false});
-    
-  
-    material = new THREE.ShaderMaterial( {
-
+    let material = new THREE.ShaderMaterial( {
         uniforms: uniforms,
         vertexShader: document.getElementById( 'vertexShader' ).textContent,
         fragmentShader: document.getElementById( 'fragmentShader' ).textContent
-
-    } );
+    });
 
     mesh = new THREE.Mesh( geometry, material );
-    mesh.position.set(-7,-1,10);
+    mesh.position.set(-7,-1,12);
     mesh.rotation.x = -0.4;
     scene.add( mesh );
 });
-    
-    renderer = new THREE.WebGLRenderer({alpha: true});
-    renderer.setPixelRatio( window.devicePixelRatio );
-    container.appendChild( renderer.domElement );
-    renderer.setClearColor(0x000000,1.0);
-    onWindowResize();
 
-    window.addEventListener( 'resize', onWindowResize, false );
+/* Renderer setup*/
+let renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setPixelRatio( window.devicePixelRatio );
+container.appendChild( renderer.domElement );
+renderer.setClearColor(0x000000,1.0);
 
-}
+/* On Window Resize */
+onWindowResize();
+window.addEventListener( 'resize', onWindowResize, false );
 
 function onWindowResize() {
-
     renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
 
+/* Render Loop*/
+animate();
 function animate( timestamp ) {
 
     requestAnimationFrame( animate );
 
     uniforms[ "time" ].value = timestamp / 1000;
+
     if(mesh.position.z != 1 && mesh.position.z > 1){
         mesh.position.z -= 0.05;
     }
-   
-   console.log(mesh.position.z);
-
+    
     renderer.render( scene, camera );
-
 }
